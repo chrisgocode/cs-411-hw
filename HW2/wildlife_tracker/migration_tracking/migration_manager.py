10b69a6e5089991239e4f1ce5ad08136dc210ecd
Christian Gonzalez <chrisgo@bu.edu>
2024-10-08 00:22:18 -0400

    added correct methods into each class

---
+++ HW2/wildlife_tracker/migration_tracking/migration_manager.py
@@ -0,0 +1,81 @@
+from typing import Optional, List, Any
+from wildlife_tracker.migration_tracking.migration import Migration
+from wildlife_tracker.migration_tracking.migration_path import MigrationPath
+from wildlife_tracker.habitat_management.habitat import Habitat
+
+
+class MigrationManager:
+    def __init__(self) -> None:
+        self.migrations: dict[int, Migration] = {}
+        self.migration_paths: dict[int, MigrationPath] = {}
+
+    def create_migration_path(
+        self,
+        species: str,
+        start_location: Habitat,
+        destination: Habitat,
+        duration: Optional[int] = None,
+    ) -> None:
+        pass
+
+    def remove_migration_path(self, path_id: int) -> None:
+        pass
+
+    def get_migration_path_by_id(self, path_id: int) -> Optional[MigrationPath]:
+        pass
+
+    def get_migration_paths(self) -> List[MigrationPath]:
+        pass
+
+    def get_migration_paths_by_species(self, species: str) -> List[MigrationPath]:
+        pass
+
+    def get_migration_paths_by_start_location(
+        self, start_location: Habitat
+    ) -> List[MigrationPath]:
+        pass
+
+    def get_migration_paths_by_destination(
+        self, destination: Habitat
+    ) -> List[MigrationPath]:
+        pass
+
+    def get_migration_path_details(self, path_id: int) -> dict:
+        pass
+
+    def update_migration_path_details(self, path_id: int, **kwargs) -> None:
+        pass
+
+    def schedule_migration(self, migration_path: MigrationPath) -> None:
+        pass
+
+    def cancel_migration(self, migration_id: int) -> None:
+        pass
+
+    def get_migration_by_id(self, migration_id: int) -> Optional[Migration]:
+        pass
+
+    def get_migrations(self) -> List[Migration]:
+        pass
+
+    def get_migrations_by_migration_path(
+        self, migration_path_id: int
+    ) -> List[Migration]:
+        pass
+
+    def get_migrations_by_start_date(self, start_date: str) -> List[Migration]:
+        pass
+
+    def get_migrations_by_status(self, status: str) -> List[Migration]:
+        pass
+
+    def get_migrations_by_current_location(
+        self, current_location: str
+    ) -> List[Migration]:
+        pass
+
+    def get_migration_details(self, migration_id: int) -> dict[str, Any]:
+        pass
+
+    def update_migration_details(self, migration_id: int, **kwargs: Any) -> None:
+        pass
